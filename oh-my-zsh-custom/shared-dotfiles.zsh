65e2229e8d3254d2bffae262e77ba1c5767a614f
Mirko Köster <it@mirkokoester.de>
2022-12-10 21:19:11 +0100

    Sharing config with several users on the same machine

---
+++ oh-my-zsh-custom/shared-dotfiles.zsh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+export DOTFILES_REPO="/home/dotfiles"
+
+create_new_user_with_shared_config () {
+    NEW_USER_NAME=$1; \
+    SHARED_DIR=$DOTFILES_REPO; \
+    SHARED_GROUP=shared_config; \
+    sudo adduser --groups $SHARED_GROUP $NEW_USER_NAME && \
+    sudo su -c "ln -s $SHARED_DIR/.oh-my-zsh/ ~" $NEW_USER_NAME && \
+    sudo su -c "ln -s $SHARED_DIR/.p10k.zsh ~/" $NEW_USER_NAME && \
+    sudo su -c "rm ~/.zshrc; ln -s $SHARED_DIR/.zshrc ~/" $NEW_USER_NAME && \
+    sudo su -c "mkdir -m 700 ~/.ssh;" $NEW_USER_NAME && \
+    sudo su -c "mkdir -m 700 ~/.oh-my-zsh-config;" $NEW_USER_NAME && \
+    sudo su -c "ln -s $SHARED_DIR/oh-my-zsh-custom/zsh-disable-compfix.zsh ~/.oh-my-zsh-config/" $NEW_USER_NAME && \
+    sudo usermod -s $(which zsh) $NEW_USER_NAME
+}
---
+++ oh-my-zsh-custom/zsh-disable-compfix.zsh
@@ -0,0 +1,3 @@
+#!/usr/bin/env zsh
+
+ZSH_DISABLE_COMPFIX=true
