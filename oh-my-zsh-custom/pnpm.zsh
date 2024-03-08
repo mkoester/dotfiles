afef19d835755e8a1fb7ae45acbab74ddc8ab95b
Mirko Köster <it@mirkokoester.de>
2024-03-08 19:02:04 +0100

    added aliases for pnpm

--- oh-my-zsh-custom/pnpm.zsh
+++ oh-my-zsh-custom/pnpm.zsh
@@ -4,6 +4,29 @@ case ":$PATH:" in
   *) export PATH="$PNPM_HOME:$PATH" ;;
 esac
 
+# Aliases
+
+#taken some aliases from https://github.com/ntnyq/omz-plugin-pnpm/blob/main/pnpm.plugin.zsh
+alias p='pnpm'
+
+## Dependencies
+alias pa='pnpm add'
+alias pad='pnpm add --save-dev'
+alias pap='pnpm add --save-peer'
+alias prm='pnpm remove'
+alias pi='pnpm install'
+alias pls='pnpm list'
+alias pu='pnpm update'
+alias puil='pnpm update --interactive --latest'
+
+## Run scripts
+alias pr='pnpm run'
+alias pd='pnpm run dev'
+alias pb='pnpm run build'
+alias plint='pnpm run lint'
+alias pfmt='pnpm run format'
+
+
 ###-begin-pnpm-completion-###
 if type compdef &>/dev/null; then
   _pnpm_completion () {
