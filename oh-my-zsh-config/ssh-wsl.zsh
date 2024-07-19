a5d2d005bd7335efc198c2d2a7000e4b87551f65
Mirko Köster <it@mirkokoester.de>
2024-07-19 21:37:33 +0200

    SSH WSL + git

--- oh-my-zsh-config/ssh-wsl.zsh
+++ oh-my-zsh-config/ssh-wsl.zsh
@@ -1,4 +1,5 @@
 #!/usr/bin/env zsh
 
 alias ssh-add='ssh-add.exe'
-alias ssh='ssh-add.exe -l > /dev/null || ssh-add.exe && echo -e "\e[92mssh-key(s) are now available in your ssh-agent until you lock your windows machine! \n \e[0m" && ssh.exe'
+export GIT_SSH_COMMAND='ssh-add.exe -l > /dev/null || ssh-add.exe && ssh.exe'
+alias ssh=$GIT_SSH_COMMAND
