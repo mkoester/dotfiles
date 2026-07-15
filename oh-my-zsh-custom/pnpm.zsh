ab121669bab1f1e7cd9633640dfdf7b5245ae65d
Mirko Köster <git@mirkokoester.de>
2026-07-15 12:51:26 +0200

    corrected pnpm path behavior

--- oh-my-zsh-custom/pnpm.zsh
+++ oh-my-zsh-custom/pnpm.zsh
@@ -2,8 +2,8 @@
 
 export PNPM_HOME="$HOME/.local/share/pnpm"
 case ":$PATH:" in
-  *":$PNPM_HOME:"*) ;;
-  *) export PATH="$PNPM_HOME:$PATH" ;;
+  *":$PNPM_HOME/bin:"*) ;;
+  *) export PATH="$PNPM_HOME/bin:$PATH" ;;
 esac
 
 # Aliases
